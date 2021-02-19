e776da5ee9c32052341f7b23abb48e6ac396f761
Daniel Tull <dt@danieltull.co.uk>
2021-02-19 12:00:09 +0000

    Use a single size property

--- Sources/SwiftUILayout/ContentView.swift
+++ Sources/SwiftUILayout/ContentView.swift
@@ -5,17 +5,17 @@ import UI
 
 struct ContentView<View: UI.View>: SwiftUI.View {
 
+    let size = CGSize(width: 600, height: 400)
     let view: View
     @State var opacity: Double = 0.5
 
     var body: some SwiftUI.View {
         VStack {
             ZStack  {
-                HostingView(content: view, size: CGSize(width: 600, height: 400))
+                HostingView(content: view, size: size)
                     .opacity(1-opacity)
 
-                view.swiftUI
-                    .frame(width: 600, height: 400)
+                view.swiftUI.frame(width: size.width, height: size.height)
                     .opacity(opacity)
             }
             Slider(value: $opacity,
